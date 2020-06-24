a8fbf77d5d4875553d3ce74b35bb60bfb537d6e6
Paulo Salvatore <paulo@salvatore.tech>
2020-06-24 19:23:23 -0300

    Estrutura de dados da API finalizada Pasta de referência do kotlin removida

--- app/src/main/java/com/oceanbrasil/ocean_a6_22_06_20/GitHubRepositoriesResult.kt
+++ app/src/main/java/com/oceanbrasil/ocean_a6_22_06_20/GitHubRepositoriesResult.kt
@@ -7,15 +7,15 @@ data class GitHubRepositoriesResult(
 data class Repository(
     val id: Int,
     val name: String,
-    val full_name: String
+    val full_name: String,
+    val description: String,
+    val owner: Owner,
+    val html_url: String
 )
 
-/*
-Desafio:
-
-Adicionar propriedade da classe owner na classe Repository
-Criação da classe owner, com os dados que achar relevante
-
-URL da API: https://api.github.com/repositories
-Verbo: GET
-*/
+data class Owner(
+    val id: Int,
+    val login: String,
+    val avatar_url: String,
+    val html_url: String
+)
